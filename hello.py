2cf8b9e7e45c69ad0af4eb00bd9e949ac5fe9209
weenybeenymini <alsgml9091@gmail.com>
2019-05-11 15:59:17 +0900

    final_parsing

--- hello.py
+++ hello.py
@@ -62,3 +62,22 @@ for UserChampionListRow in UserChampionListRows:
     print("Death: " + UserChampionListRow.find(class_='Death').get_text())
     print("Assist: " + UserChampionListRow.find(class_='Assist').get_text())
 
+
+characterNamesList = ['garen', 'galio', 'gangplank'] #디비에 있는 캐릭터 이름 다 넣어주기
+tireList = ['iron', 'bronze', 'silver', 'gold', '', 'diamond', 'master']
+
+for characterName in characterNamesList:
+    for tire in tireList:
+        driver.get('https://www.leagueofgraphs.com/ko/champions/counters/'+characterName+'/'+tire)
+        driver.find_element_by_xpath('//*[@id="mainContent"]/div/div[1]/div/table/tbody//td/button').click()
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+
+        DataTable = soup.find('table', class_='data_table sortable_table')
+        DataTableNames = DataTable.find_all('div', class_='txt')
+        DataTableWinRatios = DataTable.select('#mainContent > div > div:nth-child(1) > div > table > tbody > tr > td:nth-child(2) > div.progress-bar-container.show-for-small-down-custom > span')
+
+        for DataTableName, DataTableWinRatio in zip(DataTableNames,DataTableWinRatios):
+            print("이름: "+ DataTableName.span.get_text())
+            print("설명: "+ DataTableName.i.get_text())
+            print("승률: "+ DataTableWinRatio.get_text()[1:-1])
