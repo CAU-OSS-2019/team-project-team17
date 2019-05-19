7585125312c8de61558cf37f310a1ea393b84288
Kyeong Tae <bluesalt1621@gmail.com>
2019-05-20 01:14:47 +0900

    for sharing, not completed

---
+++ matching_server.h
@@ -0,0 +1,208 @@
+#ifndef __MATCHING_H__
+#define __MATCHING_H__
+
+#include "socket_server.h"
+#include <map>
+
+using namespace std;
+
+typedef struct SourceOfMatching{
+	int data1;
+	int data2;
+	int data3;
+}source_of_matching;
+
+// To do : data를 string으로 했을 때 다루는 것 해보기.
+typedef struct ResultOfMatching{
+	int data1;
+	int data2;
+	int data3;
+}result_of_matching;
+
+typedef struct MatchingQueue{
+	int clnt_cnt = 0;
+	map< int, int > clnt_sockets;
+	map< int, char* >nicknames;
+}matching_queue;
+
+class MatchingSocketServer : public SocketServer{
+	
+	/* google style */
+	public :
+		int receive_success;
+		int send_success;
+		matching_queue matchingQueue;
+		pthread_t t_id;
+		pthread_mutex_t matching_mutex;
+		pthread_mutex_t matching_queue_mutex;
+
+	public :
+		/* IF TO DO : server_ip[] 에서 오류나면 스트링으로 바꾸고 스트링으로 서버 주소
+		 *  받아서 나중에 사용할때 char *로 변환 해줄 것. */
+		MatchingSocketServer(string socket_name, int open_port, int clnt_listen_cnt)
+			: SocketServer(socket_name, open_port, clnt_listen_cnt){
+
+			this -> prepareServerSocket();
+		}
+		
+		void handleMatching(void){
+			int thread_create_success = -1;
+			mutexInit();
+
+			pthread_mutex_lock(&matching_mutex);
+			while(1){
+				acceptClient();
+				
+				pthread_mutex_lock(&matching_queue_mutex);
+				
+				matchingQueue.clnt_sockets.insert(make_pair(matchingQueue.clnt_cnt, clnt_sock));
+				// (matchingQueue.clnt_sockets)[matchingQueue.clnt_cnt] = clnt_sock;
+				char * buffer = (char *)malloc(sizeof(char)*31);
+				
+				// sizeof(buffer) 크기 측정해보기. 4byte로 나오면 안됨.
+				if(read(clnt_sock, buffer, sizeof(buffer)) == -1)
+					cout << "Error : server -- read() in handleMatching()."<<endl;
+				matchingQueue.nicknames.insert(make_pair(matchingQueue.clnt_cnt, buffer));
+
+				matchingQueue.clnt_cnt++;
+				
+				if(matchingQueue.clnt_cnt == 10){
+					Matching matching(matchingQueue);
+				}
+				pthread_mutex_unlock(&matching_queue_mutex);
+				
+				matching.runMatching();
+				
+			}
+
+			Matching matching(matchingQueue);
+			pthread_mutex_unlock(&matching_mutex);
+
+			matching.runMatching();
+		}
+		
+		void mutexInit(void){
+			pthread_mutex_init(&matching_mutex);
+			pthread_mutex_init(&matching_queue_mutex, NULL);
+		}
+
+};
+
+class Matching{
+	private:
+		// data needed for the matching.
+		source_of_matching source;
+
+		// data that gotten from the matching.
+		result_of_matching result;
+		
+		matching_queue matchingQueue;
+
+		int running_state = false;
+		MatchingSocketServer * matchingSock_p;
+	public:
+		//Constructor
+		Matching(matching_queue matchingQueueInput)
+			: matchingQueue(matchingQueueInput){}
+		
+		/* start matching */
+		bool runMatching(void){
+			cout<<"runMatching"<<endl;
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			// connect to the main server through matching socket.
+			matchingSock_p = 
+				new MatchingSocketClient("matching socket", "13.209.15.157", 8888);
+			
+			cout<<"runMatching2"<<endl;	
+
+			// send data needed for the matching.
+			matchingSock_p->sendData(source);
+
+
+			cout<<"runMatching3"<<endl;	
+
+			// get data for the matching from main server.
+			result = matchingSock_p->receiveData();
+			
+			cout<<"runMatching4"<<endl;	
+
+			exitMatching(matchingSock_p->receive_success);
+		}
+
+		void init_source(int input_data1, int input_data2, int input_data3){
+			source.data1 = input_data1;
+			source.data2 = input_data2;
+			source.data3 = input_data3;
+		}
+		
+
+		/* To do : 
+		 * free all dynamic allocation related to the matching 
+		 * or if needed, initialize member variables
+		 * and then exit matching.*/
+		void exitMatching(int receive_success){
+			cout << "--- end of matching ---" << endl;
+			
+			if (receive_success)
+				displayMatchedUserInfo();
+
+			else
+				displayMatchingFailInfo();
+		}
+
+		void displayMatchedUserInfo(){
+			cout << "data1 : " << result.data1 << "  "
+				<< " data2 :  " << result.data2 << "  " << "data3 : "<<result.data3 << "  " << endl;
+		}
+
+		void displayMatchingFailInfo(){
+			cout << "fail matching ( due to time-out, server communication failure etc..) "<< endl;
+		}
+};
+
+class UtilMatching{
+	public :
+		static void pressMatching(void){
+			cout << " press matching button " << endl;
+			cout << " ---running matching--- " << endl;
+			static matching * matching_object = new matching(1,2,3);
+			matching_object->runMatching();
+
+			delete matching_object;
+		}
+
+		static void endMatching(void){
+			cout << " end matching " << endl;
+			
+		}
+};
+
+
+
+
+
+
+#endif // __MATCHING_H__
---
+++ socket_server.h
@@ -0,0 +1,113 @@
+#ifndef __SOCKET_H__
+#define __SOCKET_H__
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <string>
+#include <pthread.h>
+#define MAX_CLNT 1000
+
+
+using namespace std;
+
+// a client socket class for a socket communication.
+class SocketServer{
+
+	protected:
+		string socket_name;
+		int opened_port;
+		int serv_sock;
+		int clnt_sock;
+
+		/* 이 서버 소켓 객체에서 연결 요청을 받을 클라이언트의 개수. */
+		int clnt_cnt_listen;
+
+		struct sockaddr_in serv_addr;
+		struct sockaddr_in clnt_addr;
+	
+		int clnt_addr_size;
+
+	protected:
+		void prepareServerSocket(void){
+			createSocket();
+			connectClient();
+			enterListenState();
+		}
+
+		/* create server socket */
+		void createSocket(void){
+			serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+
+			if(serv_sock == -1)
+				cout<< "server -- socket() error" << endl;;
+
+			memset(&serv_addr, 0, sizeof(serv_addr));
+			serv_addr.sin_family = AF_INET;
+			serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+			serv_addr.sin_port = htons(opened_port);
+		}
+
+		void connectClient(void){
+			if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+				cout << "Error : server -- bind()" << endl;
+
+		}
+
+		void enterListenState(void){
+			if(listen(serv_sock, clnt_cnt_listen) == -1)
+				cout << "Error : server -- listen()" << endl;
+		}
+
+		void acceptClient(void){
+			clnt_addr_size = sizeof(clnt_addr);
+			clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+
+			if(clnt_sock == -1)
+				cout << "Error : server -- accept()" << endl;
+			
+			else
+				cout << "Connected client IP : " << inet_ntoa(clnt_addr.sin_addr)
+					<<endl;
+		}
+		
+		/*
+		void receiveData(void){
+			str_len = read(sock, message, sizeof(message)-1);
+			if(str_len == -1)
+				cout << "read() error" << endl;
+
+			printf("Message from server:%s\n", message);
+
+			close(sock);
+			return 0;
+
+		}
+		*/
+
+	public:
+		SocketClient(string socket_name, int open_port, int clnt_listen_cnt)
+			: socket_name(socket_name), opened_port(open_port), 
+				clnt_cnt_listen(clnt_listen_cnt){}
+
+
+		string getSocketName(void){
+			return socket_name;
+		}
+
+
+		char* getSocketIp(void){
+			return target_server_ip;
+		}
+
+		int getSocketPort(void){
+			return target_server_port;
+		}
+
+};
+
+
+#endif // __SOCKET_H__
