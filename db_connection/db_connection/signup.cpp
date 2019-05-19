beb072b72038776a152bdafe503f8e48ab8b9aef
yuseon27 <ysnam19@naver.com>
2019-05-19 19:09:37 +0900

    Sign up finished

--- db_connection/db_connection/signup.cpp
+++ db_connection/db_connection/signup.cpp
@@ -17,32 +17,36 @@ using namespace std;
 #define PORTNUM 3306
 
 struct UserInfo {
-
+	
 	char nickname[30];
 	char id[30];
 	char pwd[30];
-
+	
 };
 
 void main()
-{
+{	
 	UserInfo user;
 
 	MYSQL conn;
 	MYSQL *connection = NULL;
-	MYSQL_RES *sql_result;
-	MYSQL_ROW sql_row;
 	int query_state;
 
 	char query[255];
 
-	cout << "Sign In" << endl;
+	cout << "Sign Up" << endl;
 
 	cout << "id : ";
 	cin >> user.id;
+	
+	cout << "password : ";
+	cin >> user.pwd;
+
+	cout << "nickname : ";
+	cin >> user.nickname;
 
 	mysql_init(&conn);
-
+	
 	connection = mysql_real_connect(&conn, HOST, USERNAME, PASSWORD, DBNAME, PORTNUM, NULL, 0);
 
 	if (connection == NULL) {
@@ -50,27 +54,15 @@ void main()
 		return;
 	}
 
-	sprintf(query, "SELECT * FROM login WHERE id='%s'", user.id);
+	sprintf(query, "INSERT INTO login VALUES ('%s', '%s', '%s')", user.nickname, user.id, user.pwd);
 
 	query_state = mysql_query(connection, query);
 
 	if (query_state != 0) {
-		cout << "Login failed : " << mysql_error(&conn) << endl;
+		cout << "Insertion failed" << mysql_error(&conn) << endl;
 		mysql_close(&conn);
 		return;
 	}
-
-	sql_result = mysql_store_result(connection);
-
-	while ((sql_row = mysql_fetch_row(sql_result)) != NULL) {
-		strcpy(user.nickname, sql_row[0]);
-		strcpy(user.id, sql_row[1]);
-		strcpy(user.pwd, sql_row[2]);
-	}
-
-	cout << "nickname\tid\tpassword" << endl;
-	cout << user.nickname << "\t" << user.id << "\t" << user.pwd << endl;
-
 
 	mysql_close(&conn);
 
