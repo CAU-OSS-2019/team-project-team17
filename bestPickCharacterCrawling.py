b25ba54307cc9fe913dcf0c06f06b27b04d6792f
weenybeenymini <alsgml9091@gmail.com>
2019-05-21 23:53:31 +0900

    add_platinum_all

--- bestPickCharacterCrawling.py
+++ bestPickCharacterCrawling.py
@@ -60,7 +60,7 @@ for link in soup.select('#drop-champions> ul > li > a'):
                     (%s, %s, %s)
         """
         
-        cursor.execute(sql, (characterName, tire if (tire == '') else 'platinum', base_win_rate))
+        cursor.execute(sql, (characterName, tire if (tire != '') else 'platinum', base_win_rate))
 
         driver.get('https://www.leagueofgraphs.com/ko/champions/counters/'+characterName+'/'+tire)
         driver.find_element_by_xpath('//*[@id="mainContent"]/div/div[1]/div/table/tbody//td/button').click()
@@ -82,7 +82,7 @@ for link in soup.select('#drop-champions> ul > li > a'):
                 VALUES
                     (%s, %s, %s, %s, %s)
             """
-            cursor.execute(sql, (characterName, tire, bestCharacterName, winRate, des))
+            cursor.execute(sql, (characterName, tire if (tire != '') else 'platinum', bestCharacterName, winRate, des))
 
 db.commit()
 
