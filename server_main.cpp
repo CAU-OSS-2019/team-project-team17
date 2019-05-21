92c5f6a41d6fcbc9cf00e7262587eb2e726b6d1d
Kyeong Tae <bluesalt1621@gmail.com>
2019-05-21 12:47:07 +0900

    add server_main.cpp

---
+++ server_main.cpp
@@ -0,0 +1,8 @@
+#include "matching_server.h"
+
+int main(void){
+	MatchingSocketServer matchingSocketServer("server matching socket", 8888, 1000);
+	matchingSocketServer.handleMatching();
+	
+	return 0;
+}
